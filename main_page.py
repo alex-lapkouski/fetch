388be14a6aed03d71bae197f32b63415331ad285
alex-lapkouski <59882296+alex-lapkouski@users.noreply.github.com>
2024-07-24 11:24:02 -0400

    Update main_page.py
    code optimization

--- main_page.py
+++ main_page.py
@@ -8,3 +8,18 @@ class MainPage:
         self.reset_button = page.get_by_text('Reset', exact=True)
         self.weighings = page.locator('css=li')
         self.success_message = page.locator('text=Yay! You find it!')
+
+    def check_the_group(self, page, needed_group, left_bowl, right_bowl, main_page):
+        main_page.reset_button.click()
+        left_bowl[0].fill(str(needed_group[0]))
+        right_bowl[0].fill(str(needed_group[1]))
+        main_page.weigh_button.click()
+        page.wait_for_selector('css=li:nth-child(2)')
+        weighings = main_page.weighings.all()
+        if "=" in weighings[1].text_content():
+            needed_number = needed_group[2]
+        elif ">" in weighings[1].text_content():
+            needed_number = needed_group[1]
+        elif "<" in weighings[1].text_content():
+            needed_number = needed_group[0]
+        return needed_number
